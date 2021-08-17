7bbd9c22abaa42de3c9fb1be2402838d4410d8cc
Vikash <vikashlp9030@gmail.com>
2021-08-17 19:36:32 +0530

    my bookhub project

---
+++ app/src/main/java/com/codewithvikash/bookhub/DashboardRecyclerAdapter.kt
@@ -0,0 +1,4 @@
+package com.codewithvikash.bookhub
+
+class DashboardRecyclerAdapter {
+}
--- app/src/main/java/com/codewithvikash/bookhub/MainActivity.kt
+++ app/src/main/java/com/codewithvikash/bookhub/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.codewithvikash.bookhub
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.FrameLayout
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var coordinatorLayout: CoordinatorLayout
-    private lateinit var toolbar: Toolbar
+   // private lateinit var toolbar: Toolbar
     private lateinit var frameLayout: FrameLayout
     private lateinit var navigationView: NavigationView
+    var previousMenuItem:MenuItem?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,19 +25,107 @@ class MainActivity : AppCompatActivity() {
 
         drawerLayout = findViewById(R.id.drawerLayout)
         coordinatorLayout = findViewById(R.id.coordinatorLayout)
-        toolbar = findViewById(R.id.toolbar)
-        frameLayout = findViewById(R.id.frame)
+//        frameLayout = findViewById(R.id.frame)
         navigationView = findViewById(R.id.navigationViwe)
-        setUpToolbar()
-    }
-    private fun setUpToolbar() {
-        setSupportActionBar(toolbar)
-        supportActionBar?.title = "Toolbar Title"
+        //setUpToolbar()
+
+        openDashboard()
+
+        /*val actionBarDrawerToggle = ActionBarDrawerToggle{
+            ativity: this@MainActivity,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        }*/
+        //drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        //actionBarDrawerToggle.syncState()
+       navigationView.setNavigationItemSelectedListener {
+           if(previousMenuItem!=null){
+               previousMenuItem?.isChecked=false
+           }
+           it.isCheckable =true
+           it.isChecked=true
+           previousMenuItem = it
+
+            when(it.itemId){
+                R.id.dashboard ->{
+                     supportFragmentManager.beginTransaction()
+                         .replace(R.id.frame,DashboardFragment())
+                         .addToBackStack("Dashboard")
+                         .commit()
+                    supportActionBar?.title ="Dashboard"
+                         drawerLayout.closeDrawers()
+                }
+                R.id.favourites ->{
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame,Favourites())
+                        .addToBackStack("Favourites")
+                        .commit()
+                    supportActionBar?.title = "Faourites"
+                    drawerLayout.closeDrawers()
+                }
+                R.id.profile ->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame,Profile())
+                        .addToBackStack("Profile")
+                        .commit()
+                    supportActionBar?.title = "Profile"
+                    drawerLayout.closeDrawers()
+
+                }
+                R.id.about_app ->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame,AboutApp())
+                        .addToBackStack("AboutApp")
+                        .commit()
+                    supportActionBar?.title ="About App"
+                    drawerLayout.closeDrawers()
+
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
     }
 
-    private fun setSupportActionBar(toolbar: Toolbar) {
-        TODO("Not yet implemented")
+    fun setUpToolBar(){
+        var toolbar = null
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Toolbar Title"
+        val suppportAction = null
+        suppportAction?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+  fun openDashboard(){
+      val fragment = DashboardFragment()
+      val transaction = supportFragmentManager.beginTransaction()
+          .replace(R.id.frame,DashboardFragment())
+          .addToBackStack("Dashboard")
+          .commit()
+      supportActionBar?.title ="Dashboard"
+      navigationView.setCheckedItem(R.id.dashboard)
+  }
+
+    override fun onBackPressed() {
+        val frag = supportFragmentManager.findFragmentById(R.id.frame)
+        when(frag){
+            !is DashboardFragment ->openDashboard()
+            else ->super.onBackPressed()
+
+        }
+    }
+
+}
+
+private fun Nothing?.setHomeButtonEnabled(b: Boolean) {
 
 
 }
